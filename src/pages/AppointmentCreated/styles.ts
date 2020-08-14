9e85424bcfeadd4e784f8ec4730c8f23b5fa76b4
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-13 22:38:58 -0300

    base of AppointmentCreated

---
+++ src/pages/AppointmentCreated/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native';
+
+const Welcome = styled.Text`
+  font-size: 22px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin-bottom: 24px;
+`;
+
+const Username = styled.Text`
+  font-size: 14px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+const Email = styled.Text`
+  font-size: 10px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin-bottom: 24px;
+`;
+
+export { Welcome, Username, Email };
