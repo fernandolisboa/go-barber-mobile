30d12a53d3f8b624caa577274a9b1fc395decbaa
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-14 15:54:06 -0300

    cleaned base code

--- src/pages/AppointmentCreated/styles.ts
+++ src/pages/AppointmentCreated/styles.ts
@@ -1,23 +1,5 @@
 import styled from 'styled-components/native';
 
-const Welcome = styled.Text`
-  font-size: 22px;
-  color: #f4ede8;
-  font-family: 'RobotoSlab-Medium';
-  margin-bottom: 24px;
-`;
+const Container = styled.View``;
 
-const Username = styled.Text`
-  font-size: 14px;
-  color: #f4ede8;
-  font-family: 'RobotoSlab-Medium';
-`;
-
-const Email = styled.Text`
-  font-size: 10px;
-  color: #f4ede8;
-  font-family: 'RobotoSlab-Medium';
-  margin-bottom: 24px;
-`;
-
-export { Welcome, Username, Email };
+export { Container };
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
 
 import { useAuth } from '../../hooks/auth';
 
-import Button from '../../components/Button';
-
-import { Welcome, Username, Email } from './styles';
+import { Container } from './styles';
 
 const Profile: React.FC = () => {
   const { user, signOut } = useAuth();
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 72,
-      }}
-    >
-      <Welcome>Olá, {user.name}</Welcome>
-      <Username>Seu username: {user.username}</Username>
-      <Email>{user.email}</Email>
-
-      <Button onPress={signOut}>Sair</Button>
-    </View>
-  );
+  return <Container />;
 };
 
 export default Profile;
