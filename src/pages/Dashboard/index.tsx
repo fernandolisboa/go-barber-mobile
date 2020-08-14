9bb1ea35a57a094b0b7c696574e0d06c53ae1c39
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-14 15:09:28 -0300

    getting & rendering providers in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,9 @@
-import React, { useCallback } from 'react';
-
+import React, { useCallback, useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+
+import Icon from 'react-native-vector-icons/Feather';
+import api from '../../services/api';
+
 import { useAuth } from '../../hooks/auth';
 
 import Button from '../../components/Button';
@@ -12,14 +15,41 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
+  ProvidersListTitle,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
 } from './styles';
+
+export interface IProvider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
   const { user, signOut } = useAuth();
 
+  const [providers, setProviders] = useState<IProvider[]>([]);
+
+  useEffect(() => {
+    api.get<IProvider[]>('providers').then(({ data }) => setProviders(data));
+  }, []);
+
   const { navigate } = useNavigation();
 
   const navigateToProfile = useCallback(() => navigate('Profile'), [navigate]);
+
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointment', { providerId });
+    },
+    [navigate],
+  );
 
   return (
     <Container>
@@ -34,7 +64,34 @@ const Dashboard: React.FC = () => {
         </ProfileButton>
       </Header>
 
-      <Button onPress={signOut}>Sair</Button>
+      <ProvidersList
+        data={providers}
+        keyExtractor={({ id }) => id}
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        renderItem={({ item: { id, name, avatar_url } }) => (
+          <ProviderContainer onPress={() => navigateToCreateAppointment(id)}>
+            <ProviderAvatar source={{ uri: avatar_url }} />
+
+            <ProviderInfo>
+              <ProviderName>{name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+
+                <ProviderMetaText>Segunda à sexta</ProviderMetaText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+
+                <ProviderMetaText>8h às 18h</ProviderMetaText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
+      />
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RectButton } from 'react-native-gesture-handler';
+
+import { IProvider } from './index';
 
 const Container = styled.View`
   flex: 1;
@@ -35,4 +39,68 @@ const UserAvatar = styled.Image`
   border-radius: 28px;
 `;
 
-export { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar };
+const ProvidersList = styled(FlatList as new () => FlatList<IProvider>)`
+  padding: 32px 24px 16px;
+`;
+
+const ProvidersListTitle = styled.Text`
+  font-size: 24px;
+  margin-bottom: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+const ProviderContainer = styled(RectButton)`
+  background: #3e3b47;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 16px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const ProviderAvatar = styled.Image`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+`;
+
+const ProviderInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+const ProviderName = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  color: #f4ede8;
+`;
+
+const ProviderMeta = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+const ProviderMetaText = styled.Text`
+  margin-left: 8px;
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+  ProvidersList,
+  ProvidersListTitle,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
+};
